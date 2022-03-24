732640ad5745c87c8743f7b9c686e08c3bebad44
Storiwr <storiwr@users.noreply.github.com>
2022-03-24 12:23:46 -0400

    Initial Commit

---
+++ Word Scramble/Word_ScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_ScrambleApp.swift
+//  Word Scramble
+//
+//  Created by Stephen on 3/24/22.
+//
+
+import SwiftUI
+
+@main
+struct Word_ScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
